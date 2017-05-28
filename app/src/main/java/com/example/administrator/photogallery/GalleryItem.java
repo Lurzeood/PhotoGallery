60c6529aad95cea17a799c89450bea76aef5df68
Lurzeood <Lurzeood@163.com>
2017-05-28 22:35:43 +0800

    完成JSON解析

---
+++ app/src/main/java/com/example/administrator/photogallery/GalleryItem.java
@@ -0,0 +1,43 @@
+package com.example.administrator.photogallery;
+
+/**
+ * Created by Lurzeood on 2017/5/28 0028.
+ */
+
+public class GalleryItem {
+
+    private String mCaption;
+
+    private String mId;
+
+    private String mUrl;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getmCaption() {
+        return mCaption;
+    }
+
+    public void setmCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getmId() {
+        return mId;
+    }
+
+    public void setmId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,59 @@
+package com.example.administrator.photogallery;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Lurzeood on 2017/5/28 0028.
+ */
+
+public class PhotoGalleryFragment extends Fragment {
+
+    private static final String TAG = "PhotoGalleryFragment";
+
+    private RecyclerView mPhotoRecyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        PhotoGalleryFragment fragment = new PhotoGalleryFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchItemsTask().execute();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_photo_gallery,container,false);
+        mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+
+        return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            new FlickrFetchr().fetchItems();
+            return null;
+        }
+    }
+
+}
