8baaca0cab818d4cb790242f34e2148ebcb855f2
Lurzeood <Lurzeood@163.com>
2017-06-16 19:18:13 +0800

    完成通知栏的显示

--- app/src/main/java/com/example/administrator/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/example/administrator/photogallery/PhotoGalleryActivity.java
@@ -1,9 +1,15 @@
 package com.example.administrator.photogallery;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 public class PhotoGalleryActivity extends SinglefragmentActivity {
 
+
+    public static Intent newIntent(Context context){
+        return new Intent(context,PhotoGalleryActivity.class);
+    }
 
     @Override
     protected Fragment createFragment() {
